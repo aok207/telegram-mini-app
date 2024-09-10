1dd08641ce2d7e3fe4e038a0113d8f3a75045017
aok207 <aungookhant007@gmail.com>
2024-09-10 22:24:17 +0630

    basic ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,16 @@
+import LinkForm from "./components/LinkForm";
+
 const App = () => {
-  return <p className="text-5xl font-bold">Hello World</p>;
+  return (
+    <div className="font-inter w-full h-full flex flex-col gap-14">
+      <h1 className="font-bold text-3xl text-center mt-24">
+        Contact <span className="text-purple-600">Crawl</span>
+      </h1>
+      <div className="w-3/4 mx-auto">
+        <LinkForm />
+      </div>
+    </div>
+  );
 };
 
 export default App;
